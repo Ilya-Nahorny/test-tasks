f781f55deb9578dba288d92ea0504db81adee93f
Ilya Nahorny <ilyanahorny@Ilyas-MacBook-Pro.local>
2023-11-17 12:32:20 +0100

    task-1

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function createPhoneNumber(numbers){
     if(numbers.length !== 10) alert('Please enter 10 numbers');
     else {
          phoneNumber = `(${numbers.slice(0,3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6,10).join('')}`
+         return phoneNumber;
     }
-  console.log(phoneNumber);
-  }
+}
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) );
+// createPhoneNumber(numbers) ;
